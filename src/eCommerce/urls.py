0b92a1ec19136e1152e8d5b296d66ce57d572b58
Karolina Mamczarz <karolina.mamczarz@op.pl>
2017-06-10 19:58:02 +0200

    Creating other template

--- src/eCommerce/urls.py
+++ src/eCommerce/urls.py
@@ -23,8 +23,9 @@ from profiles import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
+    url(r'^about/$', views.about, name='about'),
 ]
 
 if settings.DEBUG:
-    urlpatterns == static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns == static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- src/profiles/views.py
+++ src/profiles/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    context=locals()
-    template='home.html'
-    return render(request,template,context)
+    context = locals()
+    template = 'home.html'
+    return render(request, template, context)
+
+def about(request):
+    context = locals()
+    template = 'about.html'
+    return render(request, template, context)
