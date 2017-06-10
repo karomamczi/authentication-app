a6bfe901c4f81fa30a28141cafb1226e747f31d7
Karolina Mamczarz <karolina.mamczarz@op.pl>
2017-06-10 17:28:05 +0200

    Preparing formulars for profiles

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -2,7 +2,12 @@ from django.db import models
 
 # Create your models here.
 class profile(models.Model):
-    name = models.CharField(max_length=120)
+    user = models.CharField(max_length=120)
+    description = models.TextField(null=True)
+    location = models.CharField(max_length=120, blank=True, null=True)
+    job = models.CharField(max_length=120, blank=True, null=True)
 
-    def __unicode__(self):
-        return self.name
+    # Remember to use __str__ above. It is for Python 3.5 (our env). __unicode__ is for Python 2.7
+    def __str__(self):
+        return self.user
+
